083316caf8ce081e06ee4cbc3ab2eb33f28ebe0a
Ecobanoglu <ertbihal@gmail.com>
2020-11-18 21:11:36 +0300

    ilk-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import kareAl, { topla, cikar, carp as c, bol as b } from "./modules/myModule";
+
+console.log(kareAl(4));
+console.log(topla(1, 2));
+console.log(cikar(7, 3));
+console.log(c(7, 3));
+console.log(b(7, 3));
 
 function App() {
   return (
@@ -9,12 +17,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p></p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ src/modules/myModule.js
@@ -0,0 +1,29 @@
+export const topla = (a, b) => {
+  return a + b;
+};
+/* Kısa hali */
+//export const topla = (a,b) => a + b;
+
+export const cikar = (a, b) => {
+  return a - b;
+};
+/* Kısa hali */
+//export const cikar = (a,b) => a - b;
+
+export const carp = (a, b) => {
+  return a * b;
+};
+
+export const bol = (a, b) => {
+  return a / b;
+};
+/*
+const kareAl = (a) => {
+  return a * a;
+};
+*/
+/* Kısa Hali */
+const kareAl = (a) => a * a;
+
+/* Varsayılan kareAl oluyor */
+export default kareAl;
